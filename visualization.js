c1051d9e710d5b62b5ad2ab73e18bf30be0dc6a2
Lizbeth Zarate-Hernandez <lizbethzarate-hernandez@dhcp-130-58-99-55.swarthmore.edu>
2022-10-03 10:07:13 -0400

    setting up the canvas

---
+++ visualization.js
@@ -0,0 +1,9 @@
+;(function () {
+    //earches for an HTML element that has the id of gameCanvas.
+    // Once it finds the element, we can then manipulate it with JavaScript.
+    const canvas = document.getElementById('test-canvas')
+    //context is our toolbox of paintbrushes and shapes. 
+    //The 2D context contains the set of tools we want.
+    const ctx = canvas.getContext('2d')
+  
+  })()
