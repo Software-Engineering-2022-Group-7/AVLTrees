77b5478678eab94d16ca3b978dc42d82a21622dd
xdong1 <xdong1@swarthmore.edu>
2022-10-17 14:36:40 -0400

    made links open on new tab and updated implementation.html pg3 (js structure)

--- implementation.js
+++ implementation.js
@@ -4,3 +4,10 @@ function expand() {
 function collapse() {
     $('.methods_display').find('.collapse').collapse('hide');
 }
+
+function expand2() {
+    $('.methods_display2').find('.collapse').collapse('show');
+}
+function collapse2() {
+    $('.methods_display2').find('.collapse').collapse('hide');
+}
