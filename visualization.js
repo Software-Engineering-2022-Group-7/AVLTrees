2f7c41a5d8d24b42508cde6af9b49a7d402f0f4d
Lizbeth Zarate-Hernandez <lizbethzarate-hernandez@dhcp-130-58-99-55.swarthmore.edu>
2022-10-17 13:09:48 -0400

    added function to clear canvas

--- visualization.js
+++ visualization.js
@@ -1,9 +1,15 @@
 ;(function () {
-    //earches for an HTML element that has the id of gameCanvas.
+    //searches for an HTML element that has the id of canvas.
     // Once it finds the element, we can then manipulate it with JavaScript.
-    const canvas = document.getElementById('test-canvas')
+    const canvas = document.getElementById('canvas')
     //context is our toolbox of paintbrushes and shapes. 
     //The 2D context contains the set of tools we want.
     const ctx = canvas.getContext('2d')
   
   })()
+  //function that is called to clear the canvas
+    function clearcanvas(){
+    var canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
