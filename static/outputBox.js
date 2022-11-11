644a34336c64699926c712b09595fc5940ac743f
Lizbeth Zarate-Hernandez <lizbethzarate-hernandez@dhcp-130-58-99-55.swarthmore.edu>
2022-11-11 10:51:30 -0500

    creating output dialog

---
+++ static/outputBox.js
@@ -0,0 +1,8 @@
+/*
+Explaination box requirements
+Report when a node is being added and deleted
+Mention the rotations that is taking place
+Descriptions will be ongoing and should the user be able to scroll back
+
+Template:
+*/
