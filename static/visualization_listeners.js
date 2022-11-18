0dfe1bde28e81a27992b744bb833074a893edaba
xdong1 <xdong1@swarthmore.edu>
2022-11-18 01:15:02 -0500

    add listener file

---
+++ static/visualization_listeners.js
@@ -0,0 +1,35 @@
+// Execute Input/Delete when users click Enter
+document.getElementById("numInput").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("InputBtn").click();
+    }
+});
+
+document.getElementById("numDelete").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("DeleteBtn").click();
+    }
+});
+
+// Delete content after input
+document.getElementById("InputBtn").addEventListener("click", function handleClick(event) {
+    event.preventDefault();
+    const numInput = document.getElementById("numInput");
+    numInput.value = "";
+    numInput.placeholder = "Enter Key";
+});
+
+document.getElementById("DeleteBtn").addEventListener("click", function handleClick(event) {
+    event.preventDefault();
+    const numDelete = document.getElementById("numDelete");
+    numDelete.value = "";
+    numDelete.placeholder = "Enter Key";
+});
+
+// Speed bar
+document.getElementById("SpeedRange").oninput = function() {
+    framePerMovement = framePerSecond * this.value * 0.01;
+    document.getElementById("SpeedIndication").innerHTML = parseFloat(framePerMovement).toFixed(2);
+}
