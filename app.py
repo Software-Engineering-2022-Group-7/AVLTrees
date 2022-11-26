330bbc073a3bf273ee6d1d502ab17496919ec9f4
Anarsaikhan Tuvshinjargal <anarsaikhan@dhcp-130-58-161-81.swarthmore.edu>
2022-11-26 11:21:59 -0500

    quiz memes

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
